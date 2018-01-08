8a20142a9811188219d032c05725fba2432c65da
codrinon <stefan.onutu@gmail.com>
2018-01-09 01:11:31 +0200

    Initial commit

---
+++ locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpLocust, TaskSet
+
+
+def tweets(l):
+    l.client.get("/v1/api/news")
+
+
+class UserBehavior(TaskSet):
+    tasks = {tweets: 2}
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 60000
